4613ab6c248ba397ef6a6c72941d7a98213a2ef1
PiyoPiyo4 <pio_giovanni@yahoo.com>
2024-03-06 15:24:25 +1100

    env

--- reset_password.py
+++ reset_password.py
@@ -4,8 +4,12 @@ from sendgrid.helpers.mail import Mail, From, To, Subject, Content
 import random
 from flask_cors import CORS, cross_origin
 from python_http_client.exceptions import HTTPError
+from dotenv import load_dotenv
+
+load_dotenv()
 # SENDGRID_API_KEY = os.getenv("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")
-# api_key = os.getenv("YOUR_API_KEY")
+api_key = os.getenv("SENDGRID_API_KEY")
+
 
 def generate_otp():
   otp = ''.join(random.choices('0123456789', k=4))
@@ -13,7 +17,8 @@ def generate_otp():
 
 @cross_origin(origin='*',headers=['Content-Type','Authorization'])
 def send_email(recipient_email, subject, content):
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    print(os.environ.get("SENDGRID_API_KEY"))
+    sg = sendgrid.SendGridAPIClient(api_key)
     message = Mail(
         from_email=From("yaallo.noreply@gmail.com", "yaallO"),
         to_emails=To(recipient_email),
@@ -25,6 +30,8 @@ def send_email(recipient_email, subject, content):
         response = sg.client.mail.send.post(request_body=message.get())
     except HTTPError as e:
         print(e.to_dict)
+        return '1'
+    
     if response.status_code == 202:
         print("Email sent successfully!")
         return '1'
